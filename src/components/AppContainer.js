6217435033517098819e955dcbf385aa2aa2a7cb
luancurti <luancurti@gmail.com>
2018-03-20 17:58:57 -0300

    Added list of pokemons

--- src/components/AppContainer.js
+++ src/components/AppContainer.js
@@ -3,17 +3,28 @@ import React, { Component } from 'react'
 import { fetchPokemons } from '../services/poke-service'
 
 class AppContainer extends Component {
+  constructor () {
+    super()
+    this.state = {
+      pokemons: []
+    }
+  }
+
   componentDidMount () {
-    fetchPokemons()
-      .then(res => {
-        console.log(res)
-      })
+    fetchPokemons ()
+      .then(response => response.data.results)
+      .then(pokemons => this.setState({ pokemons }))
   }
 
   render () {
+    const { pokemons } = this.state
+
     return (
       <div className='App'>
-        <h1>Ola mundo</h1>
+        <h1>Lista de Pokemons</h1>
+        <ul>
+          {pokemons.map((pokemon, index) => <li key={index}>{pokemon.name}</li>)}
+        </ul>
       </div>
     )
   }
