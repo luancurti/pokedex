ea5f7e85ab756fd6974c34b89777739eab0ca9af
luancurti <luancurti@gmail.com>
2018-03-22 13:14:53 -0300

    Refactored css to responsive

--- src/components/PokemonDetail/PokemonDetail.js
+++ src/components/PokemonDetail/PokemonDetail.js
@@ -1,6 +1,7 @@
 import React, { Component, Fragment } from 'react'
 
 import { getPokemonsById } from '../../services/poke-service'
+import './index.scss'
 
 class PokemonDetail extends Component {
   constructor () {
@@ -21,11 +22,11 @@ class PokemonDetail extends Component {
     return (
       <Fragment>
         {!!pokemon && !!pokemon.sprites && !!pokemon.sprites.front_default &&
-          <section className='detail-view'>
-            <img src={pokemon.sprites.front_default} className='sprite-image' alt='sprite' />
-            <div className='data-wrapper'>
-              <h1 className='data-name'>ID: {pokemon.id} {name}</h1>
-              <p className='data-char'>Tipo: {pokemon.types[0].type.name}</p>
+          <section className='poke-detail'>
+            <img src={pokemon.sprites.front_default} className='poke-detail__sprite-image' alt='sprite' />
+            <div className='poke-detail__info-wrapper'>
+              <h1 className='poke-detail__data-name'>Nome: {pokemon.name}</h1>
+              <p className='poke-detail__data-char'>Tipo: {pokemon.types[0].type.name}</p>
             </div>
           </section>
         }
