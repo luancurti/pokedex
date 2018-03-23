0dd36555d3d08bd274efb29cddd3189b4f90c520
luancurti <luancurti@gmail.com>
2018-03-23 08:47:09 -0300

    Changed webpack configuration

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -1,12 +1,75 @@
-var path = require('path')
-var merge = require('webpack-merge')
-var dev = require('./webpack.dev')
-var UglifyJsPlugin = require('uglifyjs-webpack-plugin')
+const path = require('path');
+const webpack = require('webpack');
+const publicPath = '../dist/';
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
-module.exports = merge(dev, {
+const extractSass = new ExtractTextPlugin({
+  filename: '[name].[contenthash].css',
+  disable: process.env.NODE_ENV === 'development'
+});
+
+module.exports = {
+  //Content
+  entry: ['babel-polyfill', path.resolve(__dirname, '../src/index.js')],
+  // A SourceMap without column-mappings ignoring loaded Source Maps.
+  devtool: 'cheap-module-source-map',
+
   plugins: [
-    new UglifyJsPlugin({
-      test: /\.js($|\?)/i
-    })
-  ]
-})
+    extractSass
+  ],
+
+  output: {
+    path: path.join(__dirname, publicPath),
+    filename: '[name].bundle.js',
+    publicPath: publicPath,
+    sourceMapFilename: '[name].map',
+  },
+
+  mode: 'production',
+
+  devServer: {
+    port: 3000,
+    host: 'localhost',
+    //Be possible go back pressing the "back" button at chrome
+    historyApiFallback: true,
+    noInfo: false,
+    stats: 'minimal',
+    publicPath: publicPath,
+    contentBase: path.join(__dirname, publicPath),
+    //hotmodulereplacementeplugin
+    hot: true
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: extractSass.extract({
+          use: [
+            {
+              loader: 'css-loader'
+            },
+            {
+              loader: 'sass-loader'
+            }
+          ],
+          fallback: 'style-loader'
+        })
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: ['file-loader']
+      },
+      {
+        test: /\.js|.jsx?$/,
+        exclude: /(node_modules)/,
+        loaders: ['babel-loader']
+      },
+      {
+        test: /\.html$/,
+        exclude: /(node_modules)/,
+        loaders: ['html-loader']
+      }
+    ]
+  }
+}
